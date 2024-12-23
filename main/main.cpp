a85e2527737f2e3e1ede0253724f2e316d234b93
MBrosik <michal.bros1@gmail.com>
2024-12-23 22:53:52 +0100

    mqtt messages handling not working. Meeeeeeh

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,7 @@
 #include "gatt_client.h"
-#include "wifi_manager.h"
 #include "http_server.h"
+#include "mqtt_manager.h"
+#include "wifi_manager.h"
 #include <iostream>
 
 extern "C" void app_main(void) {
@@ -12,8 +13,35 @@ extern "C" void app_main(void) {
     std::cout << "NVS got erased" << std::endl;
   }
 
-  GattClient client;
+  // GattClient client;
 
-  WifiManager::AP::start();
-  HttpServer server;
+  // WifiManager::AP::start();
+  // HttpServer server;
+
+  WifiManager::STA::save_credentials("SkyLink 49", "SkyBros49");
+  WifiManager::STA::start();
+
+  MQTTManager mqtt_manager("mqtt://mqtt.eclipseprojects.io");
+  while (!WifiManager::STA::is_connected()) {
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+  // mqtt_manager.set_message_callback(
+  //     [](const std::string &topic, const std::string &message) {
+  //       printf("Received message: %s on topic: %s\n", message.c_str(),
+  //              topic.c_str());
+  //     });
+  mqtt_manager.set_message_callback(
+      [](const std::string &topic, const std::string &message) {
+        printf("Received message: %s on topic: %s\n", message.c_str(),
+               topic.c_str());
+      });
+  mqtt_manager.start_connecting();
+
+  while (!mqtt_manager.is_connected()) {
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+
+  mqtt_manager.subscribe("my_topic");
+
+  mqtt_manager.publish("my_topic", "Hello from ESP32");
 }
---
+++ main/modules/mqtt/mqtt_manager.h
@@ -0,0 +1,61 @@
+#ifndef MQTT_MANAGER_H
+#define MQTT_MANAGER_H
+#include <esp_log.h>
+#include <functional>
+#include <mqtt_client.h>
+#include <string>
+
+
+class MQTTManager {
+public:
+  using MessageCallback =
+      std::function<void(const std::string &topic, const std::string &message)>;
+
+  MQTTManager();
+  MQTTManager(const std::string &broker_uri, const std::string &username,
+              const std::string &password);
+  MQTTManager(const std::string &broker_uri);
+  ~MQTTManager() = default;
+
+  void start_connecting();
+  bool is_connected() const;
+  void disconnect();
+  void subscribe(const std::string &topic);
+  void unsubscribe(const std::string &topic);
+  void publish(const std::string &topic, const std::string &message);
+
+  void set_message_callback(const MessageCallback &callback);
+  MessageCallback message_callback = nullptr;
+
+private:
+  esp_mqtt_client_config_t mqtt_cfg = {
+      .broker =
+          {
+              .address =
+                  {
+                      .uri = "mqtt://192.168.43.210",
+                  },
+          },
+      // .broker.address.uri = "mqtt://mqtt.eclipseprojects.io",
+      .credentials =
+          {
+              .username = "weather_user",
+              .authentication =
+                  {
+                      .password = "zaq1@WSX",
+                  },
+          },
+      .network =
+          {
+              .timeout_ms = 10000,
+          },
+  };
+  esp_mqtt_client_handle_t client = nullptr;
+  bool connected = false;
+
+  static void
+  event_handler(void *handler_args, esp_event_base_t base, int32_t event_id,
+                esp_mqtt_event_handle_t event_data); // std::string broker_;
+};
+
+#endif // MQTT_MANAGER_H
