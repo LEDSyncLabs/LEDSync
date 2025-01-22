cfcbc1e0336c347534066193306491cb94318759
Krzysztof Wrona <rubikon02@gmail.com>
2025-01-22 01:25:34 +0100

    only receiver

--- main/modules/input/ir.hpp
+++ main/modules/input/ir.hpp
@@ -1,34 +1,34 @@
-#pragma once
+// #pragma once
 
-#include "driver/rmt_rx.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include "esp_log.h"
-#include <functional>
-#include <vector>
+// #include "driver/rmt_rx.h"
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "freertos/queue.h"
+// #include "esp_log.h"
+// #include <functional>
+// #include <vector>
 
-class IR {
-public:
-    using Callback = std::function<void(const rmt_rx_done_event_data_t&)>;
+// class IR {
+// public:
+//     using Callback = std::function<void(const rmt_rx_done_event_data_t&)>;
 
-    IR();
-    ~IR();
+//     IR();
+//     ~IR();
 
-    void addListener(gpio_num_t pin, Callback callback);
-    void start();
+//     void addListener(gpio_num_t pin, Callback callback);
+//     void start();
 
-private:
-    struct Listener {
-        gpio_num_t pin;
-        Callback callback;
-        rmt_channel_handle_t rx_channel;
-    };
+// private:
+//     struct Listener {
+//         gpio_num_t pin;
+//         Callback callback;
+//         rmt_channel_handle_t rx_channel;
+//     };
 
-    static void ir_task(void* arg);
-    static bool rmt_rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata, void *user_data);
+//     static void ir_task(void* arg);
+//     static bool rmt_rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata, void *user_data);
 
-    static QueueHandle_t gpio_evt_queue;
-    static std::vector<Listener> listeners;
-    static rmt_symbol_word_t raw_symbols[64]; // Buffer for RMT symbols
-};
+//     static QueueHandle_t gpio_evt_queue;
+//     static std::vector<Listener> listeners;
+//     static rmt_symbol_word_t raw_symbols[64]; // Buffer for RMT symbols
+// };
