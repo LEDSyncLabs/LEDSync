46ac4ddb2415d6cbf04ecbabe19ef26490e339f9
Spookyless <spookyless.info@gmail.com>
2025-01-23 17:43:16 +0100

    remove dependency

--- server/main/main.cpp
+++ server/main/main.cpp
@@ -6,7 +6,6 @@
 #include "input/input.hpp"
 #include "menu/menu.h"
 #include "mic/ADCSampler.h"
-#include "mic/I2SMEMSSampler.h"
 #include "nvs.h"
 #include "nvs_flash.h"
 #include <inttypes.h>
@@ -102,33 +101,35 @@ extern "C" void app_main(void) {
   // Input::encoder.addListener(PIN_ENCODER_RIGHT, PIN_ENCODER_LEFT,
   //  encoder_rotated);
 
-  // Input::potentiometer.addListener(PIN_POTENTIOMETER_LEFT,
-  //                                  potentiometer_changed, 500);
-  // Input::potentiometer.addListener(PIN_POTENTIOMETER_RIGHT,
-  //                                  potentiometer_changed, 500);
+  Input::potentiometer.addListener(PIN_POTENTIOMETER_LEFT,
+                                   potentiometer_changed, 500);
+  Input::potentiometer.addListener(PIN_POTENTIOMETER_RIGHT,
+                                   potentiometer_changed, 500);
 
   // Input::ir.addListener(ir_handler);
 
-  i2s_config_t adcI2SConfig = {
-      .mode =
-          (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
-      .sample_rate = MIC_SAMPLE_RATE,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-      .communication_format = I2S_COMM_FORMAT_I2S_LSB,
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-      .dma_buf_count = 4,
-      .dma_buf_len = 1024,
-      .use_apll = false,
-      .tx_desc_auto_clear = false,
-      .fixed_mclk = 0};
+  // i2s_config_t adcI2SConfig = {
+  //     .mode =
+  //         (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX |
+  //         I2S_MODE_ADC_BUILT_IN),
+  //     .sample_rate = MIC_SAMPLE_RATE,
+  //     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+  //     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+  //     .communication_format = I2S_COMM_FORMAT_I2S_LSB,
+  //     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+  //     .dma_buf_count = 4,
+  //     .dma_buf_len = 1024,
+  //     .use_apll = false,
+  //     .tx_desc_auto_clear = false,
+  //     .fixed_mclk = 0};
 
-  adcSampler = new ADCSampler(ADC_UNIT_1, PIN_MIC_OUT, adcI2SConfig);
+  // adcSampler = new ADCSampler(ADC_UNIT_1, PIN_MIC_OUT, adcI2SConfig);
 
-  TaskHandle_t adcWriterTaskHandle;
-  adcSampler->start();
-  xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler, 1,
-                          &adcWriterTaskHandle, 1);
+  // TaskHandle_t adcWriterTaskHandle;
+  // adcSampler->start();
+  // xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler,
+  // 1,
+  //                         &adcWriterTaskHandle, 1);
 
   Input::start();
 }
